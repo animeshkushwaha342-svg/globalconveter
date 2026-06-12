fc27fa3e76187988422928bae76b6156feff4dfe
animeshkushwaha342-svg <animeshkushwaha342@gmail.com>
2026-06-12 17:26:25 +0530

    Update GlobalCurrencyConverter.java 2

--- GlobalCurrencyConverter.java
+++ GlobalCurrencyConverter.java
@@ -7,6 +7,8 @@ public class GlobalCurrencyConverter {
     public static void main(String[] args) {
         
         System.out.println("Hello guys"); 
+         System.out.println("welcome"); 
+        
         
     }
 }
